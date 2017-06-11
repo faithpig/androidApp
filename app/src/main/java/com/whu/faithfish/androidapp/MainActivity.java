fff76b4064d1438b850cc53da8d8af604a2395f1
faithfish <xcxcfish@gmail.com>
2017-06-11 21:56:18 +0800

    activity的生命周期

--- app/src/main/java/com/whu/faithfish/androidapp/MainActivity.java
+++ app/src/main/java/com/whu/faithfish/androidapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.whu.faithfish.androidapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final static String MSG_NAME = "com.whu.faithfish.androidapp";
-
+    protected final static String MSG_NAME = "com.whu.faithfish.androidapp";
+    private static final String TAG = "MainActivity";//logt+Tab键自动生成
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this,"add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.del_item:
+                Toast.makeText(this,"del",Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            default:
+        }
+        return true;
+    }
+
     public void sendMessage(View view){
-        Intent intent = new Intent(this, SecondActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String msg = editText.getText().toString();
-        intent.putExtra(MSG_NAME,msg);
+//        Intent intent = new Intent(this, SecondActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.edit_message);
+//        String msg = editText.getText().toString();
+//        intent.putExtra(MSG_NAME,msg);
+//        startActivity(intent);
+//        Intent intent = new Intent("com.whu.faithfish.ACTION_START");
+//        intent.addCategory("com.whu.faithfish.MY_CATEGORY");
+//        startActivity(intent);
+        Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:10086"));
         startActivity(intent);
     }
 }
--- app/src/main/java/com/whu/faithfish/androidapp/SecondActivity.java
+++ app/src/main/java/com/whu/faithfish/androidapp/SecondActivity.java
@@ -3,8 +3,10 @@ package com.whu.faithfish.androidapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -18,5 +20,14 @@ public class SecondActivity extends AppCompatActivity {
         textView.setText(content);
         ViewGroup vg = (ViewGroup) findViewById(R.id.activity_second);
         vg.addView(textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SecondActivity.this,"生成520！!",Toast.LENGTH_LONG).show();
+                TextView txtv = (TextView) view;
+                txtv.setText("520!");
+            }
+        });
+
     }
 }
